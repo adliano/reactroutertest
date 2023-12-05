5aed111de2ac39ef4ee61f49fb9671c37d027ba1
Adliano Alves <adliano@me.com>
2023-12-05 00:08:31 -0800

    finally

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import './App.css'
+import { Home, Blog, About } from './pages'
+
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <BrowserRouter basename='/reactroutertest'>
+          <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route path='/blog' element={<Blog />}></Route>
+            <Route path='/about' element={<About />}></Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
+// reactroutertest
+export default App
